a82a2d122758088c243757a26bfe8779201c5859
Harsh97 <harsh97.au@gmail.com>
2020-06-12 14:07:53 +0930

    JS file

---
+++ script.js
@@ -0,0 +1,5 @@
+var age = prompt("Enter Your Age?");
+var days = age * 365;
+// if we want to count leaf years than use 365.25 insted of 365
+console.log("You alive around " + days + " days"); 
+alert("You alive around " + days + " days");
